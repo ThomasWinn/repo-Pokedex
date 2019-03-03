18112292cd5290d136f4bf3a274f3b70b12d60ed
Thomas Nguyen <tpnguyen1999@gmail.com>
2019-03-02 21:39:24 -0600

    Nothing is lining up at all

---
+++ PokedexMain.js
@@ -0,0 +1,14 @@
+var pokemonList = [Bulbasaur, Ivysaur, Venasaur,
+                  Charmander, Charmeleon, Charizard,
+                  Squirtle, Wartortle, Blastoise];
+
+var pokemonImg = [001Bulbasaur, 002Ivysaur, 003Venasaur,
+                  004Charmander, 005Charmeleon, 005Charizard
+                  006Squirtle, 007Wartortle, 009Blastoise];
+
+
+function PokemonCard {
+  for(var i = 0; i < pokemonList; i++) {
+    
+  }
+}
